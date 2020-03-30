9bd83ad6d87a46ad684878dfdaec8a0a3dfbf031
sc <peterchenshen@gmail.com>
2020-03-30 17:35:44 +0800

    Update run.sh

--- how-to-create-ca/run.sh
+++ how-to-create-ca/run.sh
@@ -3,6 +3,7 @@ set -x
 
 mkdir dummyca; cd dummyca
 
+# Create a root certificate
 mkdir -p rootca/newcerts
 echo 1011 > rootca/serial
 touch rootca/index.txt
@@ -13,6 +14,7 @@ openssl req -config rootca/openssl.cfg -new -x509 -key rootca/RootCA.key -out ro
 
 ls rootca/RootCA.*
 
+# Create a signing certificate, which is based on previously created root certificate
 mkdir -p sigca/newcerts
 echo 2011 > sigca/serial
 touch sigca/index.txt
@@ -25,6 +27,8 @@ openssl ca -config rootca/openssl.cfg -keyform pem -keyfile rootca/RootCA.key -c
 
 ls sigca/SigCA.*
 
+# Now the signing certificate SigCA.pem is ready and a signature (PKCS7, deattched, in DER format) can be created:
 echo abc > abc.txt
 openssl smime -sign -signer sigca/SigCA.pem -inkey sigca/SigCA.key -binary -in abc.txt -outform der -out abc.txt.p7
+# This is how it can be locally verified
 openssl smime -verify -inform der -in abc.txt.p7 -content abc.txt -CAfile rootca/RootCA.pem
